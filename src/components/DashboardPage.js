24199514bb4245ead08fa202c0b839d20038ac5d
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2019-07-22 22:17:04 -0400

    search text input fix

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -7,20 +7,20 @@ const DashboardPage = () => (
     <div>
       <h1 className="search-bar__heading">Book unique homes and experiences</h1>
     </div>
-    <div className="search-criteria">
-      <div className="search-bar__input-group">
-        <label htmlFor="searchText" className="search__input-label">
-          Where
-        </label>
-        <input
-          type="text"
-          className="search__input"
-          placeholder="Anywhere"
-          name="searchText"
-        />
-      </div>
-      {/* <button className="round-btn">Search</button> */}
+    {/* <div className="search-criteria"> */}
+    <div className="search-bar__input-group">
+      <label htmlFor="searchText" className="search__input-label">
+        Where
+      </label>
+      <input
+        type="text"
+        className="search__input"
+        placeholder="Anywhere"
+        name="searchText"
+      />
     </div>
+    {/* <button className="round-btn">Search</button> */}
+    {/* </div> */}
     <div className="content">
       <SearchResultsPage />
       <main className="hotel-view">
